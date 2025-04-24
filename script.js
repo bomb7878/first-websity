24e357113d59364832e9eece6008c93c8ce45597
bomb7878 <Nikita785nikit@gmail.com>
2025-04-24 11:24:24 +0300

    новые изменения

---
+++ script.js
@@ -0,0 +1,6 @@
+// const body = document.body
+// body.style.backgroundImage = "url('krasivaia-priroda.jpg')"
+// body.style.backgroundSize = "cover"
+// const container = document.getElementById("container")
+// container.style.backgroundColor = "rgba(255,255,255,0.5)"
+// container.style.fontWeight = "bold"
